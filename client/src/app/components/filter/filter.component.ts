76ec51584adef9b58d0d8146a528cc00d608b443
Natalija9 <asanovic.natalija@gmail.com>
2021-12-26 16:59:02 +0100

    dodata forma za izbor filtera

--- client/src/app/components/filter/filter.component.ts
+++ client/src/app/components/filter/filter.component.ts
@@ -1,6 +1,7 @@
 import { Cocktail } from 'src/app/models/cocktail.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, FormControl, Validators, ValidationErrors, FormArray } from '@angular/forms';
 
 declare const $: any;
 
@@ -13,15 +14,57 @@ declare const $: any;
 export class FilterComponent implements OnInit {
 
   @Input() cocktails: Cocktail[] | null;
+  filterForm: FormGroup;
+  categories: String[] = ["Ordinary_Drink", "Cocktail", "Milk / Float / Shake", "Cocoa", "Shot",
+    "Coffee / Tea", "Homemade_Liqueur", "Punch / Party_Drink", "Beer", "Soft_Drink / Soda"];
+  selectedCategories: String[];
+  glasses: String[] = ["Cocktail glass", "Highball glass", "OldFashioned Glass"];
+
+  get categoriesFormArray(): FormArray {
+    return this.filterForm.get("categoriesForm") as FormArray;
+  }
+
+  get glassesFormArray(): FormArray {
+    return this.filterForm.get("glassesForm") as FormArray;
+  }
 
 
   constructor() {
     this.cocktails = [];
-   }
+    this.filterForm = new FormGroup({
+      alcoholic: new FormControl('both', []),
+      categoriesForm: new FormArray([]),
+      glassesForm: new FormArray([])
+    });
+    this.selectedCategories = [];
+    this.addCheckboxes();
+  }
+
+  private addCheckboxes() {
+    this.categories.forEach(() => this.categoriesFormArray.push(new FormControl(false)));
+    this.glasses.forEach(() => this.glassesFormArray.push(new FormControl(false)));
+  }
+
 
   ngOnInit(): void {
+    $('.ui.radio.checkbox').checkbox();
+  }
+
+  onFilter(): void {
+    const data = this.filterForm.value;
+    console.log(data.alcoholic);
+
+    // this.selectedCategories = this.findNutritionistForm.value.categories
+    //     .map((checked, i) => checked ? this.categories[i] : null)
+    //     .filter(v => v !== null);
 
 
+
+    this.selectedCategories = data.categoriesForm;
+    console.log(this.selectedCategories);
+
+    const selectegGlasses = data.glassesForm;
+    console.log(selectegGlasses);
   }
 
 }
